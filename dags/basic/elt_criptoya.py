49ba0deb3a62043170808984de78d53533a92a58
diegorojasucv <darb302@gmail.com>
2024-09-05 19:27:11 -0300

    create etl functions

---
+++ dags/basic/elt_criptoya.py
@@ -0,0 +1,65 @@
+from datetime import datetime, timedelta
+
+from airflow.decorators import dag
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
+
+from cosmos import DbtTaskGroup, ProjectConfig, RenderConfig
+
+from include.profiles import airflow_db
+from include.constants import jaffle_shop_path, venv_execution_config
+
+from functions.extract_data import fetch_data_from_api
+from functions.transform_data import transform_data
+from functions.load_data import load_data_to_postgres
+
+db_config = {
+      
+    'host': 'postgres',
+    'port': 5432,
+    'dbname': 'postgres',
+    'user': 'postgres',
+    'password': 'postgres',
+    'schema': 'dbt',
+    
+}
+
+@dag(
+    schedule_interval="@daily",
+    start_date=datetime(2023, 1, 1),
+    catchup=False,
+    tags=["simple"],
+)
+def elt_criptoya() -> None:
+    """
+    The simplest example of using Cosmos to render a dbt project as a TaskGroup.
+    """
+    fetch_task = PythonOperator(
+        task_id='fetch_data_from_api',
+        python_callable=fetch_data_from_api,
+    )
+
+    transform_task = PythonOperator(
+        task_id='transform_data',
+        python_callable=transform_data,
+        op_kwargs={'data': "{{ ti.xcom_pull(task_ids='fetch_data_from_api') }}"},
+    )
+
+    load_task = PythonOperator(
+        task_id='load_data_to_postgres',
+        python_callable=load_data_to_postgres,
+        op_kwargs={'df_json': "{{ ti.xcom_pull(task_ids='transform_data') }}", 'db_config': db_config},
+    )
+
+    dbt_task = DbtTaskGroup(
+        group_id="my_jaffle_shop_project",
+        project_config=ProjectConfig(jaffle_shop_path),
+        profile_config=airflow_db,
+        execution_config=venv_execution_config,
+        render_config=RenderConfig(emit_datasets=False),
+    )
+
+    fetch_task >> transform_task >> load_task >> dbt_task
+
+
+elt_criptoya()
---
+++ dags/functions/extract_data.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+def fetch_data_from_api(**kwargs):
+    api_url = "https://criptoya.com/api/usdt/ars/100"
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status()
+        data = response.json()
+        return data
+    except requests.exceptions.RequestException as e:
+        return None
