80f051bd7e62835aeb393c7e91f30b157d776d10
diegorojasucv <darb302@gmail.com>
2024-09-06 20:47:47 -0300

    transform date fields

--- dags/functions/transform_data.py
+++ dags/functions/transform_data.py
@@ -12,9 +12,6 @@ def transform_usdt_from_criptoya_api(data, **kwargs):
     df = df.T.reset_index()
     df["updated_at"] = pd.to_datetime(df["time"], unit="s").astype(str)
     df["extracted_at"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    df = df[
-        ["index", "ask", "totalAsk", "bid", "totalBid", "updated_at", "extracted_at"]
-    ]
     df_json = df.to_json()
     return df_json
 
@@ -32,16 +29,17 @@ def transform_mep_usd_from_criptoya_api(data, **kwargs):
     mep_lede_48hs = data_dict["mep"]["lede"]["24hs"]
 
     data = {
-        "MEP al30 CI": mep_al30_ci,
-        "MEP gd30 CI": mep_gd30_ci,
-        "MEP lede CI": mep_lede_ci,
-        "MEP al30 24 hs": mep_al30_48hs,
-        "MEP gd30 24 hs": mep_gd30_48hs,
-        "MEP lede 24 hs": mep_lede_48hs,
+        "mep al30 ci": mep_al30_ci,
+        "mep gd30 ci": mep_gd30_ci,
+        "mep lede ci": mep_lede_ci,
+        "mep al30 24 hs": mep_al30_48hs,
+        "mep gd30 24 hs": mep_gd30_48hs,
+        "mep lede 24 hs": mep_lede_48hs,
     }
 
     df = pd.DataFrame.from_dict(data_dict)
     df = df.T.reset_index()
+    df["updated_at"] = pd.to_datetime(df["timestamp"], unit="s").astype(str)
     df["extracted_at"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     df_json = df.to_json()
     return df_json
@@ -52,7 +50,10 @@ def transform_other_usd_from_criptoya_api(data, **kwargs):
     data_dict = ast.literal_eval(data)
     df = pd.DataFrame.from_dict(data_dict)
     df = df[["ahorro", "tarjeta", "blue"]]
+    df = df[df.index.isin(["price", "timestamp", "ask", "bid"])]
     df = df.T.reset_index()
+    df["updated_at"] = pd.to_datetime(df["timestamp"], unit="s").astype(str)
+    df["extracted_at"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     df_json = df.to_json()
     return df_json
 
@@ -61,6 +62,7 @@ def transform_bcra_from_api(data, **kwargs):
 
     data_dict = ast.literal_eval(data)
     df = pd.DataFrame.from_dict(data_dict)
+    df["updated_at"] = pd.to_datetime(df["fecha"]).astype(str)
     df["extracted_at"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     df_json = df.to_json()
     return df_json
