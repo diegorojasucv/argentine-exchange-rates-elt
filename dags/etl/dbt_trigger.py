76cd9bcd232ef13d9f8f775e50b0d42c667bf2b1
diegorojasucv <darb302@gmail.com>
2024-10-01 16:30:14 -0300

    update dbt trigger etl

--- dags/etl/dbt_trigger.py
+++ dags/etl/dbt_trigger.py
@@ -1,18 +1,43 @@
 """DAG that triggers a dbt task group for executing dbt commands"""
 
-from cosmos import DbtDag, ProjectConfig, RenderConfig
+from types import NoneType
+
+from airflow.decorators import dag
+from cosmos import DbtTaskGroup, ProjectConfig, RenderConfig
 
 from include.constants import jaffle_shop_path, venv_execution_config
 from include.profiles import redshift_db
 
-dbt_run_task = DbtDag(
-    dag_id="dbt_run",
-    project_config=ProjectConfig(jaffle_shop_path),
-    profile_config=redshift_db,
-    operator_args={"install_deps": True},
-    execution_config=venv_execution_config,
-    render_config=RenderConfig(select=["path:models"], emit_datasets=False),
+
+@dag(
     dag_id="dbt_trigger",
     catchup=False,
     tags=["dbt_trigger"],
 )
+def dbt_trigger() -> NoneType:
+    """
+    This function sets up and runs a dbt task group using the provided
+    configurations for the dbt project, profile, and virtual environment execution.
+    It is designed to be scheduled and managed by Airflow.
+
+    Task Details:
+        - dbt_task: Trigger a build run. This means that a run job and a test job are executed.
+
+    Returns:
+        None: This DAG doesn't return any values.
+    """
+    dbt_task = DbtTaskGroup(
+        group_id="dbt_project",
+        project_config=ProjectConfig(jaffle_shop_path),
+        profile_config=redshift_db,
+        operator_args={"install_deps": True},
+        execution_config=venv_execution_config,
+        render_config=RenderConfig(
+            select=["+metrics_exchange_rates"], emit_datasets=False
+        ),
+    )
+
+    dbt_task
+
+
+dbt_trigger()
