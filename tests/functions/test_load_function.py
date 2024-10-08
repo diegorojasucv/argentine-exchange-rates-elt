0d1955c6ad42ed83043311087ac88b0bd82c8447
Diego Rojas <67651418+diegorojasucv@users.noreply.github.com>
2024-10-08 17:40:03 -0300

    Revert "move functions folder to root project"

--- tests/functions/test_load_function.py
+++ tests/functions/test_load_function.py
@@ -1,15 +1,12 @@
-from unittest.mock import MagicMock, patch
-
-import pandas as pd
 import pytest
-
-from functions.load_data import (connect_to_redshift_engine,
-                                 load_data_to_redshift)
+import pandas as pd
+from unittest.mock import patch, MagicMock
+from dags.functions.load_data import connect_to_redshift_engine, load_data_to_redshift
 
 
 @pytest.fixture
 def mock_create_engine():
-    with patch("functions.load_data.create_engine") as mock:
+    with patch("dags.functions.load_data.create_engine") as mock:
         yield mock
 
 
@@ -46,7 +43,7 @@ def test_load_data_to_redshift(mock_create_engine, sample_json_data):
     """
     mock_engine = mock_create_engine.return_value
 
-    with patch("functions.load_data.pd.DataFrame.to_sql") as mock_to_sql:
+    with patch("dags.functions.load_data.pd.DataFrame.to_sql") as mock_to_sql:
         load_data_to_redshift(sample_json_data, "mock_table")
 
         mock_to_sql.assert_called_once_with(
--- tests/functions/test_transform_function.py
+++ tests/functions/test_transform_function.py
@@ -1,14 +1,14 @@
+import pytest
+import pandas as pd
 import textwrap
 from datetime import datetime
 from io import StringIO
-
-import pandas as pd
-import pytest
-
-from functions.transform_data import (transform_bcra_from_api,
-                                      transform_mep_usd_from_criptoya_api,
-                                      transform_other_usd_from_criptoya_api,
-                                      transform_usdt_from_criptoya_api)
+from dags.functions.transform_data import (
+    transform_usdt_from_criptoya_api,
+    transform_mep_usd_from_criptoya_api,
+    transform_other_usd_from_criptoya_api,
+    transform_bcra_from_api,
+)
 
 
 @pytest.fixture
